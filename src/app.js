a7693dc2fa7069172de41061371a153cbcd68397
Paloma Pérez de Madrid <paloma.perezmadridlaguna@usp.ceu.es>
2024-11-23 18:11:20 +0100

    login sin bd

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var profileRouter = require('./routes/profile');
+var loginRouter = require('./routes/login');
 //var indexRouter = require('./routes/admin');
 //var indexRouter = require('./routes/home');
 
@@ -24,6 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/profile', profileRouter);
+app.use('/login', loginRouter)
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
