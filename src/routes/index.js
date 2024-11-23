1e6f3156cfdbe97a14052aef070e2d469ba65042
Paloma Pérez de Madrid <paloma.perezmadridlaguna@usp.ceu.es>
2024-11-23 17:51:27 +0100

    profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/profile', (req, res) => {
+  res.render('profile'); 
+});
+
 module.exports = router;
