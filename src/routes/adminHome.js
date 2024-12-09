19ddcd823f608953a2d8576ef6b75dc4a86d3a6a
Paloma Pérez de Madrid <paloma.perezmadridlaguna@usp.ceu.es>
2024-12-09 22:13:35 +0100

    Block users falta profile usuario

--- src/routes/adminHome.js
+++ src/routes/adminHome.js
@@ -6,7 +6,7 @@ var sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('../cashme');
 
 router.get('/', function (req, res) {
-    db.all('SELECT nombre, email, photo_path, id  FROM usuarios', (err, rows) => {
+    db.all('SELECT nombre, email, photo_path, id, activo  FROM usuarios', (err, rows) => {
         if (err) {
             console.error('Error al obtener los usuarios:', err);
             res.status(500).send('Error al cargar los usuarios.');
@@ -68,6 +68,44 @@ router.delete('/delete-user/:id', (req, res) => {
         return res.status(200).json({ message: 'User deleted successfully.' });
     });
 });
+
+
+router.post('/block-active-user/:id', (req, res) => {
+    const userId = req.params.id; 
+
+    if (!userId) {
+        return res.status(400).json({ error: 'User ID is required to delete a user.' });
+    }
+
+    db.get('SELECT activo FROM usuarios WHERE id = ?', [userId], (err, row) => {
+        if (err) {
+            console.error('Error fetching user status:', err);
+            return res.status(500).json({ error: 'An error occurred while fetching the user status.' });
+        }
+
+        if (!row) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        // Cambiar el estado: Si está activo (1), se bloquea (0); si está bloqueado (0), se activa (1)
+        const newStatus = row.activo === 1 ? 0 : 1;
+
+        db.run('UPDATE usuarios SET activo = ? WHERE id = ?', [newStatus, userId], function (err) {
+            if (err) {
+                console.error('Error updating user status:', err);
+                return res.status(500).json({ error: 'An error occurred while updating the user status.' });
+            }
+
+            if (this.changes === 0) {
+                return res.status(404).json({ error: 'The user was not found.' });
+            }
+
+            console.log(`User with ID ${userId} updated to status ${newStatus}.`);
+            return res.status(200).json({message: 'User status updated successfully.'});
+        });
+    });
+});
+
 
 
 
