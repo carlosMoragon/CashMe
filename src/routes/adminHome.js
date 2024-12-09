335a91dc511fec736667b3e8cd4decf3abfc41d7
Paloma Pérez de Madrid <paloma.perezmadridlaguna@usp.ceu.es>
2024-12-09 13:37:01 +0100

    borrado users está, falta pulir css

--- src/routes/adminHome.js
+++ src/routes/adminHome.js
@@ -6,7 +6,7 @@ var sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('../cashme');
 
 router.get('/', function (req, res) {
-    db.all('SELECT nombre, email, photo_path FROM usuarios', (err, rows) => {
+    db.all('SELECT nombre, email, photo_path, id  FROM usuarios', (err, rows) => {
         if (err) {
             console.error('Error al obtener los usuarios:', err);
             res.status(500).send('Error al cargar los usuarios.');
@@ -44,5 +44,32 @@ router.get('/get-transacciones', function (req, res) {
         res.json({ fechas: fechasArray, ingresos: ingresosArray, gastos: gastosArray });
     });
 });
+
+
+router.delete('/delete-user/:id', (req, res) => {
+    const userId = req.params.id; 
+    console.log('Received User ID:', userId); // Verifica que se esté recibiendo el ID
+
+    if (!userId) {
+        return res.status(400).json({ error: 'User ID is required to delete a user.' });
+    }
+
+    db.run('DELETE FROM usuarios WHERE id = ?', [userId], function (err) {
+        if (err) {
+            console.error('Error deleting user:', err);
+            return res.status(500).json({ error: 'An error occurred while deleting the user.' });
+        }
+
+        if (this.changes === 0) {
+            return res.status(404).json({ error: 'The user was not found.' });
+        }
+
+        console.log(`User with ID ${userId} deleted successfully.`);
+        return res.status(200).json({ message: 'User deleted successfully.' });
+    });
+});
+
+
+
 
 module.exports = router;
