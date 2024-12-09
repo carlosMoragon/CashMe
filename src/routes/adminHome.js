a3dcccd3bcd63a9a20bb9384a85040fe7909feab
Paloma Pérez de Madrid <paloma.perezmadridlaguna@usp.ceu.es>
2024-12-09 08:43:28 +0100

    fotos usuarios en admin

--- src/routes/adminHome.js
+++ src/routes/adminHome.js
@@ -6,7 +6,7 @@ var sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('../cashme');
 
 router.get('/', function (req, res) {
-    db.all('SELECT nombre, email FROM usuarios', (err, rows) => {
+    db.all('SELECT nombre, email, photo_path FROM usuarios', (err, rows) => {
         if (err) {
             console.error('Error al obtener los usuarios:', err);
             res.status(500).send('Error al cargar los usuarios.');
